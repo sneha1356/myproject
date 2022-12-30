14bf7f50e4b585834bb99674026b9ed60492edd2
sneha1356 <snehayeddula1356@gmail.com>
2022-12-30 12:29:27 +0530

    Mysql project1

--- project/signup.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-from Database import Mysql_Connector
-from tkinter import *
-class Sign_Up():
-    def __init__(self):
-        db_obj = Mysql_Connector(host="localhost", user='root', passwd='password', db='hcl')
