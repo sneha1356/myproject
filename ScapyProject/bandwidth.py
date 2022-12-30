27ddf8e9d83f2d08ab69313cf664bc2f9f049131
sneha1356 <snehayeddula1356@gmail.com>
2022-12-30 13:06:14 +0530

    delete menu

--- ScapyProject/bandwidth.py
+++ ScapyProject/bandwidth.py
@@ -44,3 +44,27 @@ label_download_header=tk.Label(text="Download:",font="")
 label_download_header.pack()
 label_download=tk.Label(text="Calculating..",font="Quicksand 12")
 label_download.pack()
+def upadte():
+    global last_upload,last_download,upload_speed,down_speed
+    counter=net_io_counters()
+    upload=counter.bytes_sent
+    download=counter.bytes_recv
+    total=upload+download
+    if last_upload>0:
+        if upload<last_upload:
+            upload_speed=0
+        else:
+            upload_speed=upload-last_upload
+    if last_download>0:
+        if download<last_download:
+            down_speed=0
+        else:
+            down_speed=download-last_download
+    last_upload=upload
+    last_download=download
+    label_total_upload["text"]=f"{size(upload)} ({upload} Bytes)"
+    label_total_download["text"]=f"{size(download)}({download} Bytes)"
+    label_total_usage["text"]=f"{size(total)}\n"
+    label_upload["text"]=size(upload_speed)
+    label_download["text"]=size(down_speed)
+
