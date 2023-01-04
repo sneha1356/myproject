2205af5901662485b6e36e7d562d713443bc576f
sneha1356 <snehayeddula1356@gmail.com>
2023-01-04 13:08:01 +0530

    paramiko

--- Paramiko/Statistics.py
+++ Paramiko/Statistics.py
@@ -5,6 +5,7 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname="10.0.2.15",username="siri",password="1356")
 stdin,stdout,stderr,=ssh.exec_command("netstat --listening|less")
 df=pd.DataFrame(stdout)
+
+print(df)
 print(df.columns)
 df.to_excel('df.xlsx')
-print(df)
